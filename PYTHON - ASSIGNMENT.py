62498ee0d956801a30c749d728ccc2a53f849cc7
Pradesh06 <118629357+Pradesh06@users.noreply.github.com>
2022-11-19 23:54:27 +0530

    Add files via upload

---
+++ PYTHON - ASSIGNMENT.py
@@ -0,0 +1,44 @@
+# write a program to find even and odd numbers.
+
+def login():
+    Agri=int(input("enter the number"))
+    if (Agri%2==0):
+        print("its an even number")
+    
+    else:
+        print("its an odd number")
+    
+login()
+
+
+# Create a login function.
+
+def login():
+    username = input("Please enter your username: ")
+    password = input("Please enter your password: ")
+    if username == "Pradesh" and password == "6789":
+        print("Hello, Pradhu!")
+    else:
+        print("Invalid username or password.")
+
+login()
+
+
+# create a list of fruits and iterate through them.
+
+fruits = ["Peach", "Plum", "Pear", "Apricot", "Strawberry"]
+
+for x in fruits:
+  print(x)
+
+
+
+        
+
+    
+    
+ 
+    
+ 
+
+        
